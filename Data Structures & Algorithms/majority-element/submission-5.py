2a56a162bed50c7a8e00d0c7e66d26c2d94e1fd1
MyleCutie45 <141122968+MyleCutie45@users.noreply.github.com>
2026-06-07 00:34:57 +0700

    Add: majority-element - submission-5

---
+++ Data Structures & Algorithms/majority-element/submission-5.py
@@ -0,0 +1,16 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        # n = len(nums)
+        # count_max = 0
+        # dfd = defaultdict(int)
+        # for i in range(n):
+        #     dfd[nums[i]]+=1
+        #     count_max = max(dfd[nums[i]],count_max)
+        # for num,count in dfd.items():
+        #     if count == count_max:
+        #         return num
+        freq = Counter(nums)
+        heap = []
+        for num,count in freq.items():
+            heapq.heappush(heap,(-count,num))
+        return heapq.heappop(heap)[1]
